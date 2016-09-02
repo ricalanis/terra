b0411fa4e52d523adf7f8715b16c198bcc72fa21
Israel Zúñiga de la Mora <israelzuniga@users.noreply.github.com>
2016-09-01 19:29:56 -0500

    app.py: Update on visual style
    Compliance with PEP8 style guide.
    Also, added some docstrings

--- app.py
+++ app.py
@@ -9,10 +9,12 @@ import inegi
 
 app = Flask(__name__)
 
+
 def add_cors_headers(response):
     response.headers['Access-Control-Allow-Origin'] = '*'
     if request.method == 'OPTIONS':
-        response.headers['Access-Control-Allow-Methods'] = 'DELETE, GET, POST, PUT'
+        response.headers['Access-Control-Allow-Methods'] = 'DELETE,  \
+          GET,POST, PUT'
         headers = request.headers.get('Access-Control-Request-Headers')
         if headers:
             response.headers['Access-Control-Allow-Headers'] = headers
@@ -20,10 +22,9 @@ def add_cors_headers(response):
 
 app.after_request(add_cors_headers)
 
-###
-# Routing for your application.
-###
-
+'''
+Routing for your application.
+'''
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -36,7 +37,6 @@ def home():
         output_csv = fileops.return_csv(output_dict)
         return Response(response=output_csv, status=200, mimetype="text/csv")
 
-
     return '''
     <!doctype html>
     <title>Sube un excel como este:.</title>
@@ -47,31 +47,56 @@ def home():
     </form>
     '''
 
+
 @app.route('/about/')
 def about():
-    """Render the website's about page."""
+    '''
+    Function: about(). Renders the website's about page.
+    Route: <API>/about
+    Method: GET
+    '''
     return render_template('about.html')
 
-#/query?q=Garza%20Sada
+
 @app.route('/query')
 def query():
+    '''
+    Function:
+    Route: <API>/
+    Method: GET
+    Input parameters:
+    Usage: /
+    Example:/query?q=Garza%20Sada
+    '''
     query = request.args.get('q')
     output = inegi.call_inegi(query)
-    return Response(response=json.dumps(output), status=200, mimetype="application/json")
+    return Response(response=json.dumps(output),
+                    status=200, mimetype="application/json")
 
-#/intersection?calle1=Garza%20Sada&calle2=Acapulco&ciudad=Monterrey
+
 @app.route('/intersection')
 def intersection():
+    '''
+    Function: intersection()
+    Route: <API>/intersection
+    Method: GET
+    Input parameters: calle1, calle2, ciudad
+    Usage: /intersection?calle1=<String>&calle2=<String>&ciudad=<String>
+    Example:/intersection?calle1=Garza%20Sada&calle2=Acapulco&ciudad=Monterrey
+    '''
     street1 = request.args.get('calle1')
     street2 = request.args.get('calle2')
     city = request.args.get('ciudad')
     output = inegi.crossing(street1, street2, city)
-    return Response(response=json.dumps(output), status=200, mimetype="application/json")
+    return Response(response=json.dumps(output),
+                    status=200, mimetype="application/json")
 
 
 @app.errorhandler(404)
 def page_not_found(error):
-    """Custom 404 page."""
+    '''
+    Function: page_not_found(). Displays our famous HTTP404 error.
+    '''
     return render_template('404.html'), 404
 
 
