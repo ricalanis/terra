3642a46989aebba6908403e58f20c0a59b0e2c3f
Israel Zúñiga de la Mora <israelzuniga@users.noreply.github.com>
2016-09-01 19:31:10 -0500

    INEGI URL
    Change on INEGI URL to get inside a 79 characters row

--- inegi.py
+++ inegi.py
@@ -1,12 +1,17 @@
 import requests
 import tools
 
+URL_INEGI= "http://gaia.inegi.org.mx/NLB/tunnel/TableAliasV60/busqueda"
+
+
 def bulk_coords_convert(array):
     output = []
     for line in array:
         try:
-            response = crossing(line["calle_1"], line["calle_2"], line["municipio"])
-            current_coords = {"longitud":response["match"]["long"], "latitud":response["match"]["lat"]}
+            response = crossing(line["calle_1"],
+			  line["calle_2"], line["municipio"])
+            current_coords = {"longitud":response["match"]["long"],
+			  "latitud":response["match"]["lat"]}
             output.append(dict(line, **current_coords))
         except:
             current_coords ={"longitud":"NA","latitud":"NA"}
@@ -22,8 +27,11 @@ def crossing(street1, street2, city):
 def call_inegi(query):
     query = tools.normalize_data(query)
     request_headers={"Content-Type":"application/json"}
-    request_body = '{"idUser":"", "pagina":1, "paramProy":"", "proyName":"mdm6", "searchCriteria":"'+ query +'", "tabla":"geolocator", "where":""}'
-    r = requests.get("http://gaia.inegi.org.mx/NLB/tunnel/TableAliasV60/busqueda", headers=request_headers, data = request_body )
+    request_body = '{"idUser":"", "pagina":1, "paramProy":"",
+	  "proyName":"mdm6", "searchCriteria":"'+ query +'",
+	  "tabla":"geolocator", "where":""}'
+
+	r = requests.get(URL_INEGI, headers=request_headers, data = request_body )
     output = r.json()
     return output
 
